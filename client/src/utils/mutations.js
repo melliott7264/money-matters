d3d2cf8616a14315e9c433c61b97330b5d075f85
Mark Elliott <markelliottva@gmail.com>
2022-07-22 10:36:43 -0400

    server and client templates setup for project

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,29 @@
+import { gql } from '@apollo/client';
+
+// Variables must match variables used in callback function in components and pages.
+// Property names must match resolvers on back-end
+// Arguments must match typeDefs on back-end
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
