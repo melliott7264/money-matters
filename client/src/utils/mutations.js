ec8937d84b18145ec3a398bfa472d7a78351f57d
Mark Elliott <markelliottva@gmail.com>
2022-07-27 14:33:12 -0400

    minor updates to all graphql files

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -27,3 +27,62 @@ export const ADD_USER = gql`
     }
   }
 `;
+
+export const ADD_ARTICLE = gql`
+mutation saveArticle($articleDate: String!, $source: String!, $title: String!, $description: String!, $url: String!, $username: String!) {
+	saveArticle(articleDate: $articleDate, source: $source, title: $title, description: $description, url: $url, username: $username) {
+	_id
+	articleDate
+	postDate
+	source
+	title 
+	description
+	url
+	username
+}
+`;
+
+export const REMOVE_ARTICLE = gql`
+  mutation removeArticle($id: ID!) {
+    removeArticle(_id: $id) {
+      _id
+      title
+      username
+    }
+  }
+`;
+
+export const ADD_COMMENT = gql`
+  mutation addComment($articleId: ID!, $commentBody: String!) {
+    addComment(articleId: $articleId, commentBody: $commentBody) {
+      _id
+      articleId
+      commentBody
+      postDate
+      username
+    }
+  }
+`;
+
+export const REMOVE_COMMENT = gql`
+  mutation removeComment($id: ID!, $articleId: String!) {
+    removeComment(_id: $id, articleId: $articleId) {
+      _id
+      articleId
+    }
+  }
+`;
+
+export const EDIT_COMMENT = gql`
+mutation editComment($_id: ID!, $commentBody: String!){
+	editComment(_id: $_Id, commentBody: $commentBody) {
+		_id
+    articleId
+		commentBody
+		postDate
+		username
+		}
+	}
+}
+
+`;
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,94 @@
 import { gql } from '@apollo/client';
+
+// Query all articles for user (for the saved news page)
+export const GET_ARTICLES = gql`
+  query articles {
+    articles {
+      _id
+      userId
+      articleDate
+      postDate
+      source
+      title
+      description
+      url
+      username
+    }
+  }
+`;
+
+// Query specific article for viewing or commenting
+export const GET_ARTICLE = gql`
+  query article($id: ID!) {
+    article(_id: $id) {
+      _id
+      articleDate
+      postDate
+      source
+      title
+      description
+      URL
+      username
+      Comments {
+        _id
+        postDate
+        username
+        commentBody
+      }
+    }
+  }
+`;
+
+// Query all comments for user
+export const GET_COMMENTS = gql`
+  query comments($username: String!) {
+    comments(username: $username) {
+      _id
+      articleId
+      commentBody
+      postDate
+      username
+    }
+  }
+`;
+
+// Query specific comment
+export const GET_COMMENT = gql`
+  query comment($id: ID!) {
+    comment(_id: $id) {
+      _id
+      articleId
+      commentBody
+      postDate
+      username
+    }
+  }
+`;
+
+// Query for logged-in user
+export const GET_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      savedArticles {
+        _id
+        articleDate
+        postDate
+        source
+        title
+        description
+        url
+        username
+        comments {
+          _id
+          articleId
+          commentBody
+          postDate
+          username
+        }
+      }
+    }
+  }
+`;
