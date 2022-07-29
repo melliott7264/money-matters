0acb363567aaf778ebd758258735752694081989
Darobinson20 <Darobinson20@gmail.com>
2022-07-28 20:14:16 -0400

    started main page

--- client/src/pages/Main/main.js
+++ client/src/pages/Main/main.js
@@ -1,7 +1,25 @@
 import React from 'react';
-import './main.css';
+import Article from '../../components/Article';
+import './main.css'
 
-const Main = () => {
-    
-}
+import { useQuery } from 'apollo/client';
+import { GET_ARTICLES } from '../../utils/queries';
 
+
+const Main = () =>  {
+    const [saveArticle ] = useQuery(GET_ARTICLES);
+   
+
+    return (
+        <main>
+            <div className='flex-row justify-space-between'>
+                <div className='col-12 mb-3'>
+                    {saveArticle}
+
+                </div>
+            </div>
+        </main>
+    )
+};
+
+export default Main;
