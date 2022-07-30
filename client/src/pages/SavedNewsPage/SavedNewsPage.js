a8906fa474e7f7b87c3104d447de3379c0bd7eeb
Tyler Shifflett <tylershifflett@Tylers-MacBook-Pro.local>
2022-07-30 16:50:47 -0400

    Update Saved Page with Imports

--- client/src/pages/SavedNewsPage/SavedNewsPage.js
+++ client/src/pages/SavedNewsPage/SavedNewsPage.js
@@ -1,89 +1,79 @@
 import React, { useState, useEffect } from 'react';
-
+import Auth from '../../utils/auth';
+import { removeArticleid, saveArticleIds } from '../utils/localStorage';
+import { REMOVE_ARTICLE } from '../utils/graphql/mutations';
+import { GET_ME } from '../utils/queries';
 
 const SavedNewsPage = () => {
     const [userData, setUserData] = useState({});
-    
- 
-    const userDataLength = Object.keys(userData).length;
+    const [deleteArticle, { error }] = useMutation(REMOVE_ARTICLE);
+
+    // const userDataLength = Object.keys(userData).length;
+
+    const { loading, data } = useQuery(GET_ME);
 
     useEffect(() => {
-        const getUserData = async () => {
-            try {
-                const token = Auth.loggedIn() ? Auth.getToken() : null;
+        const user = data?.me || {};
+        setUserData(user);
+    }, [data]);
 
-                if (!token) {
-                    return false;
+    if (userData.savedArticles?.length) {
+
+        const savedArticles = userData.savedArticles.map(article => {
+            return article.articleId;
+        });
+        saveArticleIds(savedArticles);
+    }
+
+    const handleDeleteArticle = async (articleId) => {
+        try {
+            const reposne = await deleteArticle({
+                variable: { articleId: articleId },
+            });
+            if (response) {
+                removeArticleid(articleId);
             }
-
-            const response = await getMe(token);
-
-            if(!response.ok) {
-                throw new Error('Something went wrong!');
-            }
-            const user = await response.json();
-            setUserData(user);
         } catch (err) {
-        console.error(err);
+            console.log(err);
+
         }
     };
 
-    getUserData();
-  },  [userDataLength]);
-
-  const handleDeleteArticle = async (articleId) => {
-
-    const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-    if (!token) {
-        return false;
+    // Failure to upload the data you will receive this message 
+    if (!userDataLength) {
+        return <div>Loading...</div>;
     }
 
-    try {
-        const response = await deleteArticle(articleId, token);
-
-        if(!response.ok) {
-            throw new Error('Something went wrong!');
-    }
-
-    const updatedUser = await response.json();
-    setUserData(updatedUser);
-    removeArticleid(articleId);
-    } catch (err) {
-        console.error(err);
-    }
+    // Returns the saved articles
+    return (
+        <>
+            <div>
+                <h1>Saved Articles</h1>
+                <div className="card-body">
+                    <div className="panel-body">
+                        <ul className="list-group">
+                            {this.state.articles.map(article => (
+                                <Article
+                                    key={article.url}
+                                    title={article.title}
+                                    source={article.source}
+                                    url={article.url}
+                                    date={article.publishedAt}
+                                    description={article.description}
+                                    urlToImage={article.urlToImage}
+                                />
+                                //      <Button
+                                //     className="btn-block btn-danger"
+                                //     onClick={() => handleDeleteArticle(article.articleId)}
+                                //   >Delete this Article!</Button>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
 
-// Failure to upload the data you will receive this message 
-if (!userDataLength) {
-    return <div>Loading...</div>;
-}
-
-// Returns the saved articles
-return (
-    <div>
-        <h1>Saved Articles</h1>
-        <div className="card-body">
-            <div className="panel-body">
-                <ul className="list-group">
-                    {this.state.articles.map(article => (
-                        <Article
-                            key={article.url}
-                            title={article.title}
-                            source={article.source}
-                            url={article.url}
-                            date={article.publishedAt}
-                            description={article.description}
-                            urlToImage={article.urlToImage}
-                        />
-                    ))}
-                </ul>
-
-            </div>
-        </div>
-    </div>
-
-);
-}
-
 export default SavedNewsPage;
+
