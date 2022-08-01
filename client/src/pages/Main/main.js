d90e3c2a781949bc200988364c4306311980caa2
Darobinson20 <Darobinson20@gmail.com>
2022-08-01 19:14:45 -0400

    template2

--- client/src/pages/Main/main.js
+++ client/src/pages/Main/main.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import Article from '../../components/Article';
-import Single from '../../pages/Single';
-import './main.css';
+import React, { useEffect, useState } from 'react';
+import Article from '../../components/Article/Article';
+import './main.css'
+import { useQuery, useMutation} from '@apollo/client';
+import { GET_ARTICLES, GET_ME } from '../../utils/queries';
 
-import { useQuery } from 'apollo/client';
-import { GET_ARTICLES } from '../../utils/queries';
+import {
+  Container,
+  Row,
+  
+} from 'react-bootstrap';
 
-const Main = () => {
-  const [saveArticle] = useQuery(GET_ARTICLES);
 
-  return (
-    <main>
-      <div className="flex-row justify-space-between">
-        {saveArticle && (
-          <div className="col-12 mb-3">
-            <Article />
-          </div>
-        )}
-        <Single _id={Article} />
-      </div>
-    </main>
-  );
+const Main = () =>  {
+    const [loading, saveArticle ] = useState({});
+    const [articleData, setArticleData] = useState({});
+
+    const { loading, error, data }
+   
+
+    
+  
+    return (
+      <Container>
+        <Row>
+          <Article
+            key={articleData.url}
+            title={articleData.title}
+            source={articleData.source}
+            url={articleData.url}
+            date={articleData.publishedAt}
+            description={articleData.description}
+          />
+          <Row>
+          <div className="flex-row justify-space-between">
+          {saveArticle && (
+            <div className="col-12 mb-3">
+              <Article />
+            </div>
+          )}
+          <Single _id = {Article}/>
+         
+     
+        </div>
+  </Row>
+  </Row>
+  </Container>
+
+);
+
+  
+
+
 };
 
 export default Main;
